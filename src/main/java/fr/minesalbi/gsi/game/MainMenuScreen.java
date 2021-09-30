26eb74da1be98bedfabc847cf064ca9ca5324ac2
Julien Lesbegueries <julien.lesbegueries@mines-albi.fr>
2021-09-30 15:45:15 +0200

    s

--- src/main/java/fr/minesalbi/gsi/game/MainMenuScreen.java
+++ src/main/java/fr/minesalbi/gsi/game/MainMenuScreen.java
@@ -4,16 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainMenuScreen implements Screen {
 
 	final MarioGame game;
 	OrthographicCamera camera;
-	private float mainMenuAnimationTime = 0f;
 
 	public MainMenuScreen(final MarioGame game) {
 		this.game = game;
@@ -31,15 +27,6 @@ public class MainMenuScreen implements Screen {
 		game.spriteBatch.setProjectionMatrix(camera.combined);
 		game.spriteBatch.begin();
 		
-		
-		
-		
-		this.mainMenuAnimationTime += delta/2;
-		Animation<AtlasRegion> walk = new Animation<>(1 / 30f, game.textureAtlas.findRegions("m-player-walk"), PlayMode.LOOP);
-		AtlasRegion region = walk.getKeyFrame(mainMenuAnimationTime);
-        game.spriteBatch.draw(region, mainMenuAnimationTime*150%this.camera.viewportWidth, 300, 50, 50);
-        game.spriteBatch.draw(region, mainMenuAnimationTime*100%this.camera.viewportWidth, 300, 50, 50);
-        
         
 		game.font.getData().setScale(2);
 		game.font.draw(game.spriteBatch, "Welcome to  Mario!!! ", 150, 50);
